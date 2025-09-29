a411c3bf4e30c1807b3c2ee840be4e55fe9d3b70
monosodrac <monosodrac@gmail.com>
2025-09-29 10:59:18 -0300

    add and configure redux to the project

---
+++ src/store/reducers/cart.ts
@@ -0,0 +1,23 @@
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import { type Restaurant } from '../../pages/Home';
+
+type CartState = {
+    items: Restaurant[]
+};
+
+const initialState: CartState = {
+    items: []
+};
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        add: (state, action: PayloadAction<Restaurant>) => {
+            state.items.push(action.payload)
+        }
+    }
+});
+
+export const { add } = cartSlice.actions;
+export default cartSlice.reducer
